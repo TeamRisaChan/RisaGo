46c4bf2f4346b1bb3f02b7356b449a045d89b9ff
EC2 Default User <ec2-user@ip-172-31-92-23.ec2.internal>
2019-12-26 08:24:38 +0000

    add json

---
+++ data.js
@@ -0,0 +1,47 @@
+var data = new Array();
+var pin = new Array();
+
+// XMLHttpRequestオブジェクトの作成
+// var request = new XMLHttpRequest();
+
+// // URLを開く
+// request.open('GET', "http://118.27.5.222/api/Risachan/"+"1", true);
+// request.responseType = 'json';
+
+// // レスポンスが返ってきた時の処理を記述 
+// request.onload = function () {
+//   // レスポンスが返ってきた時の処理
+//   pin=this.response;
+
+//   console.log(pin.length);
+  
+//   // import resoID from "./main.js"
+//   // console.log(resoID);
+  
+//   for(var j=0;j<pin.length;j++){
+//     data.push({
+//       lat: pin[j].lat, //緯度
+//       lng: pin[j].lon, //経度
+//       content: pin[j].name //情報ウィンドウの内容
+//     });
+//   }
+// }
+// request.send();
+
+pin=[{"id":1,"name":"アルビス 野々市三納店","lat":36.517541,"lon":136.610414},{"id":4,"name":"アルビス 明倫通り店","lat":36.531074,"lon":136.601354},{"id":7,"name":"イオン 御経塚店","lat":36.548687,"lon":136.59834},{"id":10,"name":"イオン 野々市南店","lat":36.502134,"lon":136.605318},{"id":14,"name":"Aコープ 富奥店","lat":36.510829,"lon":136.612235},{"id":17,"name":"大阪屋ショップ 野々市三納店","lat":36.516418,"lon":136.608694},{"id":20,"name":"マックスバリュ 野々市店","lat":36.5264,"lon":136.612212},{"id":25,"name":"マルエー 押野店","lat":36.545463,"lon":136.618019},{"id":30,"name":"ローソン 野々市粟田店","lat":36.5141,"lon":136.60892},{"id":37,"name":"西村酒店","lat":36.543788,"lon":136.612481},{"id":39,"name":"野々市市役所","lat":36.519626,"lon":136.609862}];
+
+
+console.log(pin.length);
+
+// import resoID from "./main.js"
+// console.log(resoID);
+// import aaa from "./map"
+// console.log(aaa);
+
+for(var j=0;j<pin.length;j++){
+  data.push({
+    lat: pin[j].lat, //緯度
+    lng: pin[j].lon, //経度
+    content: pin[j].name //情報ウィンドウの内容
+  });
+}
--- map.js
+++ map.js
@@ -1,17 +1,7 @@
+
 //最初から情報ウィンドウを表示させる
 function map_canvas() {
-  //マーカーの情報
-  var data = new Array();
-  data.push({
-    lat: '36.530187', //緯度
-    lng: '136.62784899999997', //経度
-    content: "KIT" //情報ウィンドウの内容
-  });
-  // data.push({
-  //   latlang: '35.678187',
-  //   lng: '139.76817',
-  //   content: '<span style="color:#d50000">八重洲ブックセンター</span>'
-  // });
+
   //初期位置に、上記配列の最初の緯度経度を格納
   var latlng = new google.maps.LatLng(data[0].lat, data[0].lng);
   var opts = {
