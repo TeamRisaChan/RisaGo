6a6663c8e8affe6dc1d6c1395736ccec01490c07
EC2 Default User <ec2-user@ip-172-31-92-23.ec2.internal>
2019-12-27 09:27:10 +0000

    can move

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ var pin = new Array();
 // var request = new XMLHttpRequest();
 
 // // URLを開く
-// request.open('GET', "http://118.27.5.222/api/Risachan/"+"1", true);
+// request.open('GET', "https://118.27.5.222/api/Risachan/"+"1", true);
 // request.responseType = 'json';
 
 // // レスポンスが返ってきた時の処理を記述 
@@ -28,15 +28,20 @@ var pin = new Array();
 // }
 // request.send();
 
-pin=[{"id":1,"name":"アルビス 野々市三納店","lat":36.517541,"lon":136.610414},{"id":4,"name":"アルビス 明倫通り店","lat":36.531074,"lon":136.601354},{"id":7,"name":"イオン 御経塚店","lat":36.548687,"lon":136.59834},{"id":10,"name":"イオン 野々市南店","lat":36.502134,"lon":136.605318},{"id":14,"name":"Aコープ 富奥店","lat":36.510829,"lon":136.612235},{"id":17,"name":"大阪屋ショップ 野々市三納店","lat":36.516418,"lon":136.608694},{"id":20,"name":"マックスバリュ 野々市店","lat":36.5264,"lon":136.612212},{"id":25,"name":"マルエー 押野店","lat":36.545463,"lon":136.618019},{"id":30,"name":"ローソン 野々市粟田店","lat":36.5141,"lon":136.60892},{"id":37,"name":"西村酒店","lat":36.543788,"lon":136.612481},{"id":39,"name":"野々市市役所","lat":36.519626,"lon":136.609862}];
 
-
-console.log(pin.length);
+// console.log(pin.length);
 
 // import resoID from "./main.js"
-// console.log(resoID);
+$(".chip").click(function(){
+  console.log(num)
+})
+
 // import aaa from "./map"
 // console.log(aaa);
+
+
+
+pin=[{"id":1,"name":"アルビス 野々市三納店","lat":36.517541,"lon":136.610414},{"id":4,"name":"アルビス 明倫通り店","lat":36.531074,"lon":136.601354},{"id":7,"name":"イオン 御経塚店","lat":36.548687,"lon":136.59834},{"id":10,"name":"イオン 野々市南店","lat":36.502134,"lon":136.605318},{"id":14,"name":"Aコープ 富奥店","lat":36.510829,"lon":136.612235},{"id":17,"name":"大阪屋ショップ 野々市三納店","lat":36.516418,"lon":136.608694},{"id":20,"name":"マックスバリュ 野々市店","lat":36.5264,"lon":136.612212},{"id":25,"name":"マルエー 押野店","lat":36.545463,"lon":136.618019},{"id":30,"name":"ローソン 野々市粟田店","lat":36.5141,"lon":136.60892},{"id":37,"name":"西村酒店","lat":36.543788,"lon":136.612481},{"id":39,"name":"野々市市役所","lat":36.519626,"lon":136.609862}];
 
 for(var j=0;j<pin.length;j++){
   data.push({
